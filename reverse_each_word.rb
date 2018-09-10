d29c7bf37c845aa445b6687917f5b18be5fc2274
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-10 16:25:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def reverse_each_word(string)
-  string_array = string.split
   binding.pry
+  string_array = string.split
   reversed = string_array.collect { |word| word.reverse }
   reversed.join(" ")
 end
